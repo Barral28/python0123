c700666da908e27152ed78f507eebd1f77329c39
Barral28 <luiselias.barral@unmsm.edu.pe>
2023-01-22 01:04:58 -0500

    Update ejercicio2.py
    Actualizar

--- Ejercicio 1501/ejercicio2.py
+++ Ejercicio 1501/ejercicio2.py
@@ -1,25 +1,87 @@
 ###Luis Elias Barral Larios
 Biblioteca={
-    'nombreDeInstitucion':"Colegio N",
-    'categoria':["Novela","Epico","Cientifico",],
-    'Autor':["autor1","autor2,autor3,autor4,autor5"],
-    'Libro':["libro1","libro2,libro3,libro4,autor5"],
-    'Estado':["Activo","Prestamo"],
-    
+    'categoria':["Drama","Terror","Satira",],
+   
+    'estado':["activo","prestamo"],
+   
     'alumnos':{
-        "7753":{"nombre":"juan",
+        "0001":{"nombre":"juan",
                 "apellido":"rodriguez",
+                "libro":[],
                 },
 
-        "7754":{"nombre":"juan",
+        "0002":{"nombre":"juan",
                 "apellido":"rodriguez",
+                "libro":[],
                 },
 
-        "7755":{"nombre":"juan",
+        "0003":{"nombre":"juan",
                 "apellido":"rodriguez",
+                "libro":[],
                 },
-    },
+            },
 
-    
+    'libro': {
+        "1000":{"nombre_libro":"Don quijote",
+                "nombre_autor":"Miguel cervantes",
+                "categoria":"Satira",
+                "estado":"prestado",
+                },
+
+        "1001":{"nombre_libro":"Divina comedia",
+                "nombre_autor":"Dante aliguieri",
+                "Estado":"prestado",
+                "categoria":"Drama",
+                },
+
+        "1002":{"nombre_libro":"Edgar Alan Poe",
+                "apellido":"El gato negro",
+                "estado":"activo",
+                "categoria":"Terror",
+                },
+            },
 }
+
+print("""Escribe una opción
+        1) Obtener la lista de categorías de libros.
+        2) Obtener nombres de los libros y autores.
+        3) poder cambiar el estado de un libro a prestado
+        4) Lista de usuarios de la biblioteca.
+        5) Salir""")
+opcion=input()
+
+if opcion == '1':
+    print(Biblioteca["categoria"])
+            
+elif opcion == '2':
+    codigo=input("Ingrese el codigo del libro: ")
+    listalibros=list(Biblioteca["libro"].keys())
+    if codigo in listalibros:
+        print("Libro encontrado")
+        titulo=Biblioteca["libro"][codigo]["nombre_libro"]
+        autor=Biblioteca["libro"][codigo]["nombre_autor"]
+        print(Biblioteca["libro"])
+            # print(nombre_libro)
+            # print(nombre_autor)
+        
+elif opcion =='3':
+    codigo=input("Ingrese el codigo del libro: ")
+    listalibros=list(Biblioteca["libro"].keys())
+    if codigo in listalibros:
+        print("Libro encontrado")
+        titulo=Biblioteca["libro"]["estado"]="prestado"
+        cambio=Biblioteca["libro"][codigo]["estado"]
+        print("Cambiado a ",cambio)
+    else:
+        print("Libro no encontrado")
+elif opcion =='4':  
+    lista=list()
+    lista.append(Biblioteca["alumnos"])
+    print(lista)
+else:
+    print("Comando desconocido, vuelve a intentarlo")
+       
+        
+
+
 
